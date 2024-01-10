374591e5001bb6cdbbf62a8faebf2fcad953854a
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-10 13:40:01 +0100

    buttons functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Button from "./Button";
 import FormAddFriend from "./FormAddFriend";
 import FormSplit from "./FormSplit";
 import FriendList from "./FriendList";
+import { useState } from "react";
 
 function App() {
+  const [showAddFriend, setshowAddFriend] = useState(true);
+
+  function handleShowAddPerson() {
+    setshowAddFriend((show) => !show);
+  }
   return (
     <div className='app'>
       <div className='sidebar'>
         <FriendList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddPerson}>
+          {showAddFriend ? "close" : "add person"}
+        </Button>
       </div>
       <FormSplit />
     </div>
--- src/FormSplit.js
+++ src/FormSplit.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from "./Button";
 
 const FormSplit = () => {
   return (
     <form className='form-split-bill'>
       <h2>split bill with X</h2>
+      <label>💵 Bill value</label>
+      <input type='text' />
+      <label>💷 Your expense</label>
+      <input type='text' />
+      <label>💸 X's expense</label>
+      <input type='text' disabled />
+      <label>Who is paying the bill?</label>
+      <select>
+        <option value='user'>Me</option>
+        <option value='friend'>X</option>
+      </select>
+      <Button>Split bill</Button>
     </form>
   );
 };
