d8d4b5b08717aca1f6380aae59597a20cc7a4763
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-11 10:29:06 +0100

    .

--- src/FormAddFriend.js
+++ src/FormAddFriend.js
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
 import Button from "./Button";
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
 
 const FormAddFriend = () => {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48?u=118836");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}=${id}`,
+      balance: 0,
+    };
+    setName("");
+    setImage("https://i.pravatar.cc/48?u=118836");
+    console.log(newFriend);
+  }
+
   return (
-    <form className='form-add-friend'>
+    <form className='form-add-friend' onSubmit={handleSubmit}>
       <label>👩🏿‍🤝‍👩🏿Friend name</label>
       <input
         value={name}
